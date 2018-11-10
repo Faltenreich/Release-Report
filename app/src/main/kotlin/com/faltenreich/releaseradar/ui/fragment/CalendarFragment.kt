beff9e08878410b5c9b42b5ec2ed0664cdc1cd3f
Faltenreich <philipp.fahlteich@gmail.com>
2018-11-10 20:18:18 +0100

    Updated layout of calendar

--- app/src/main/kotlin/com/faltenreich/releaseradar/ui/fragment/CalendarFragment.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/ui/fragment/CalendarFragment.kt
@@ -1,4 +1,18 @@
 package com.faltenreich.releaseradar.ui.fragment
+import android.os.Bundle
+import android.view.View
 import com.faltenreich.releaseradar.R
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.util.*
 
-class CalendarFragment : BaseFragment(R.layout.fragment_calendar)
+class CalendarFragment : BaseFragment(R.layout.fragment_calendar) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        calendarView.setDate(Date())
+    }
+}
