be66d4d016e4522441030f4ef65e41c52fa43d44
Faltenreich <philipp.fahlteich@gmail.com>
2018-12-17 18:47:37 +0100

    Fixed ripple effect on release list

--- app/src/main/kotlin/com/faltenreich/releaseradar/ui/viewholder/ReleaseViewHolder.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/ui/viewholder/ReleaseViewHolder.kt
@@ -10,13 +10,12 @@ import com.faltenreich.releaseradar.ui.fragment.ReleaseListFragmentDirections
 import kotlinx.android.synthetic.main.list_item_release.*
 
 class ReleaseViewHolder(context: Context, parent: ViewGroup) : BaseViewHolder<Release>(context, R.layout.list_item_release, parent) {
+
     override fun onBind(data: Release) {
-        release_name.text = data.artistName?.let { artist -> "$artist - ${data.name}" } ?: data.name
-        data.imageUrlForCover?.let { imageUrl -> release_cover.setImageAsync(imageUrl) } ?: release_cover.setImageResource(android.R.color.transparent)
-        data.mediaType?.let { mediaType ->
-            release_type.setImageResource(mediaType.iconResId)
-        }
         release_container.setOnClickListener { openRelease(data) }
+        data.imageUrlForCover?.let { imageUrl -> release_cover.setImageAsync(imageUrl) } ?: release_cover.setImageResource(android.R.color.transparent)
+        release_type.setImageResource(data.mediaType?.iconResId ?: android.R.color.transparent)
+        release_name.text = data.artistName?.let { artist -> "$artist - ${data.name}" } ?: data.name
     }
 
     private fun openRelease(release: Release) {
