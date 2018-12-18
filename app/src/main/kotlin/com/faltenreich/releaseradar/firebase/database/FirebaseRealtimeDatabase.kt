edba511427dce590488a0aaf3861511203fdb0af
Faltenreich <philipp.fahlteich@gmail.com>
2018-12-18 21:17:02 +0100

    Fixed initialization of Firebase database

--- app/src/main/kotlin/com/faltenreich/releaseradar/firebase/database/FirebaseRealtimeDatabase.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/firebase/database/FirebaseRealtimeDatabase.kt
@@ -5,8 +5,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 internal object FirebaseRealtimeDatabase {
 
-    private val database: FirebaseDatabase
-        get() = FirebaseDatabase.getInstance().apply { setPersistenceEnabled(true) }
+    private val database by lazy { FirebaseDatabase.getInstance().apply { setPersistenceEnabled(true) } }
 
     fun createReference(path: String): DatabaseReference = database.getReference(path)
 }
