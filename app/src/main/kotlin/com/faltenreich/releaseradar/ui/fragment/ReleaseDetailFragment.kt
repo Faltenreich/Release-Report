edc7144c878340896f60245bfb78cc1e527b7d1f
Faltenreich <philipp.fahlteich@gmail.com>
2019-01-03 22:08:18 +0100

    More dynamic chips

--- app/src/main/kotlin/com/faltenreich/releaseradar/ui/fragment/ReleaseDetailFragment.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/ui/fragment/ReleaseDetailFragment.kt
@@ -3,9 +3,12 @@ package com.faltenreich.releaseradar.ui.fragment
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import com.faltenreich.releaseradar.R
 import com.faltenreich.releaseradar.data.model.Genre
+import com.faltenreich.releaseradar.data.model.Release
 import com.faltenreich.releaseradar.data.print
 import com.faltenreich.releaseradar.data.viewmodel.ReleaseDetailViewModel
 import com.faltenreich.releaseradar.setImageAsync
@@ -38,13 +41,13 @@ class ReleaseDetailFragment : BaseFragment(R.layout.fragment_release_detail) {
                 release?.imageUrlForWallpaper?.let { url -> releaseWallpaperImageView.setImageAsync(url) } ?: releaseWallpaperImageView.setImageResource(android.R.color.transparent)
                 release?.mediaType?.let { mediaType -> collapsingToolbarLayout.setContentScrimResource(mediaType.colorResId) }
 
-                releaseDateChip.text = release?.releaseDate?.print()
-
                 val description = release?.description?.takeIf(String::isNotBlank)
-                releaseDescriptionTextView.text = description ?: getString(R.string.no_content_available)
+                releaseDescriptionTextView.text = description ?: getString(R.string.unknown_content)
                 releaseDescriptionTextView.setTypeface(releaseDescriptionTextView.typeface, if (description != null) Typeface.NORMAL else Typeface.ITALIC)
 
                 release?.let {
+                    addMeta(release)
+
                     viewModel.observeGenres(release, this) { genres ->
                         genreChipContainer.removeAllViews()
                         genres?.forEach { genre -> addGenre(genre) }
@@ -54,11 +57,21 @@ class ReleaseDetailFragment : BaseFragment(R.layout.fragment_release_detail) {
         }
     }
 
-    private fun addGenre(genre: Genre) {
-        context?.let { context ->
-            val chip = Chip(context)
-            chip.text = genre.name
-            genreChipContainer.addView(chip)
+    private fun addMeta(release: Release) {
+        addChip(metaChipContainer, release.releaseDate?.print() ?: getString(R.string.placeholder), R.drawable.ic_date)
+        addChip(metaChipContainer, release.durationInSeconds?.let { durationInSeconds -> getString(R.string.duration_placeholder).format(durationInSeconds) } ?: getString(R.string.placeholder), R.drawable.ic_duration)
+    }
+
+    private fun addGenre(genre: Genre) = addChip(genreChipContainer, genre.name)
+
+    private fun addChip(container: ViewGroup, title: String?, @DrawableRes iconResId: Int? = null) = context?.let { context ->
+        val chip = Chip(context).apply {
+            text = title
+            iconResId?.let {
+                setChipIconResource(iconResId)
+                // FIXME: Breaks chipIconTint
+            }
         }
+        container.addView(chip)
     }
 }
