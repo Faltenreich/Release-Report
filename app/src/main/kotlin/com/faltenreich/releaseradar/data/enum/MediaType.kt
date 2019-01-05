4874f75243c22301dc6289b96d3dc5439486ace2
Faltenreich <philipp.fahlteich@gmail.com>
2019-01-05 21:48:13 +0100

    Added dark colors to media type

--- app/src/main/kotlin/com/faltenreich/releaseradar/data/enum/MediaType.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/data/enum/MediaType.kt
@@ -11,11 +11,12 @@ enum class MediaType(
         override val singularStringRes: Int,
         override val pluralStringRes: Int,
         override val colorResId: Int,
+        override val colorDarkResId: Int,
         override val iconResId: Int
 ) : FirebaseEnum, NameResProvider, ColorProvider, IconProvider {
-    MOVIE("movie", R.string.movie, R.string.movies, R.color.blue, R.drawable.ic_movie),
-    MUSIC("music", R.string.music, R.string.music, R.color.red, R.drawable.ic_music),
-    VIDEO_GAME("game", R.string.video_game, R.string.video_games, R.color.green, R.drawable.ic_videogame);
+    MOVIE("movie", R.string.movie, R.string.movies, R.color.blue, R.color.blue_dark, R.drawable.ic_movie),
+    MUSIC("music", R.string.music, R.string.music, R.color.red, R.color.red_dark, R.drawable.ic_music),
+    GAME("game", R.string.game, R.string.games, R.color.green, R.color.green_dark, R.drawable.ic_game);
 
     companion object {
         fun valueForKey(key: String): MediaType? = values().firstOrNull { type -> type.key == key }
--- app/src/main/kotlin/com/faltenreich/releaseradar/data/provider/ColorProvider.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/data/provider/ColorProvider.kt
@@ -2,4 +2,5 @@ package com.faltenreich.releaseradar.data.provider
 
 interface ColorProvider {
     val colorResId: Int
+    val colorDarkResId: Int
 }
