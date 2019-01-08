8140b22b21c662d0ba5b9fbbeea05cebbe8235c7
Faltenreich <philipp.fahlteich@gmail.com>
2019-01-08 19:13:01 +0100

    Transition today button

--- app/src/main/kotlin/com/faltenreich/releaseradar/ui/adapter/SlideOutBehavior.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/ui/adapter/SlideOutBehavior.kt
@@ -12,22 +12,22 @@ class SlideOutBehavior @JvmOverloads constructor(context: Context, attrs: Attrib
     var maySlideIn = true
     var maySlideOut = true
 
+    private var margin: Float = -1f
+
     override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: View, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type)
     }
 
     override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: View, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        if (!maySlideIn) {
-            // TODO: Transition translation
-            translateWithScrollView(child, child.height.toFloat())
-        } else if (maySlideOut) {
-            translateWithScrollView(child, dy.toFloat())
+        if (margin < 0f) {
+            margin = if (child.layoutParams is ViewGroup.MarginLayoutParams) (child.layoutParams as ViewGroup.MarginLayoutParams).bottomMargin.toFloat() else 0f
         }
-    }
-
-    private fun translateWithScrollView(child: View, dy: Float) {
-        val margin = if (child.layoutParams is ViewGroup.MarginLayoutParams) (child.layoutParams as ViewGroup.MarginLayoutParams).bottomMargin.toFloat() else 0f
-        child.translationY = Math.max(0f, Math.min(child.height + margin, child.translationY + dy))
+        val offset = when {
+            !maySlideIn -> Math.min(child.translationY + 1, child.height.toFloat())
+            maySlideOut -> dy.toFloat()
+            else -> child.translationY
+        }
+        child.translationY = Math.max(0f, Math.min(child.height + margin, child.translationY + offset))
     }
 }
