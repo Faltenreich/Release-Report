b2a7e9f995ae4cd4984c5da736b37af41cbf19d0
Faltenreich <philipp.fahlteich@gmail.com>
2019-01-09 20:15:49 +0100

    Integrated CoordinatorLayout into spotlight

--- app/src/main/kotlin/com/faltenreich/releaseradar/ui/fragment/SpotlightFragment.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/ui/fragment/SpotlightFragment.kt
@@ -1,7 +1,9 @@
 package com.faltenreich.releaseradar.ui.fragment
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.doOnPreDraw
 import com.faltenreich.releaseradar.R
 import com.faltenreich.releaseradar.data.enum.MediaType
 import com.faltenreich.releaseradar.ui.adapter.ReleaseViewPagerAdapter
@@ -9,9 +11,24 @@ import kotlinx.android.synthetic.main.fragment_spotlight.*
 
 class SpotlightFragment : BaseFragment(R.layout.fragment_spotlight) {
 
+    override fun onResume() {
+        super.onResume()
+        invalidatePaddingForTranslucentStatusBar()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initLayout()
+    }
+
+    private fun invalidatePaddingForTranslucentStatusBar() {
+        view?.doOnPreDraw {
+            val frame = Rect()
+            activity?.window?.decorView?.getWindowVisibleDisplayFrame(frame)
+            appbarLayout.setPadding(0, frame.top, 0, 0)
+            searchView.setPadding(0, frame.top, 0, 0)
+            statusBarBackground.layoutParams.height = frame.top
+        }
     }
 
     private fun initLayout() {
