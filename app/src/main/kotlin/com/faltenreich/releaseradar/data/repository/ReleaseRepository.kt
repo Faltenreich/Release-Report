a8d7ecc3fbf0f13e638aa345f92cffe36443df9a
Faltenreich <philipp.fahlteich@gmail.com>
2019-02-25 19:03:38 +0100

    Fixed visibility of spotlight favorites when empty

--- app/src/main/kotlin/com/faltenreich/releaseradar/data/repository/ReleaseRepository.kt
+++ app/src/main/kotlin/com/faltenreich/releaseradar/data/repository/ReleaseRepository.kt
@@ -10,16 +10,17 @@ import org.threeten.bp.LocalDate
 object ReleaseRepository : Repository<Release, ReleaseDao>(ReleaseDao) {
 
     fun getFavorites(mediaType: MediaType? = null, from: LocalDate? = null, onResult: (List<Release>) -> Unit) {
-        val favoriteReleases = mutableListOf<Release>()
-        val favoriteReleaseIds = UserPreferences.favoriteReleaseIds
-        val onNext = { index: Int -> if (index == (favoriteReleaseIds.size - 1)) onResult(favoriteReleases.toList()) }
-        favoriteReleaseIds.forEachIndexed { index, id ->
-            getById(id, onSuccess = { release ->
-                release?.takeIf { mediaType?.let { release.mediaType == mediaType }.isTrueOrNull && from?.minusDays(1)?.isBefore(release.releaseDate).isTrueOrNull }?.let { favoriteReleases.add(release) }
-                onNext(index)
-            }, onError = {
-                onNext(index)
-            })
-        }
+        UserPreferences.favoriteReleaseIds.takeIf(Collection<*>::isNotEmpty)?.let { favoriteReleaseIds ->
+            val favoriteReleases = mutableListOf<Release>()
+            val onNext = { index: Int -> if (index == (favoriteReleaseIds.size - 1)) onResult(favoriteReleases.toList()) }
+            favoriteReleaseIds.forEachIndexed { index, id ->
+                getById(id, onSuccess = { release ->
+                    release?.takeIf { mediaType?.let { release.mediaType == mediaType }.isTrueOrNull && from?.minusDays(1)?.isBefore(release.releaseDate).isTrueOrNull }?.let { favoriteReleases.add(release) }
+                    onNext(index)
+                }, onError = {
+                    onNext(index)
+                })
+            }
+        } ?: onResult(listOf())
     }
 }
