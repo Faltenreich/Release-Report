73a88b890caa52411e98a6077bcb1e6fc8cea061
Faltenreich <philipp.fahlteich@gmail.com>
2019-06-12 19:12:00 +0200

    Renamed spotlight fragments

--- app/src/main/kotlin/com/faltenreich/release/ui/fragment/SpotlightFragment.kt
+++ app/src/main/kotlin/com/faltenreich/release/ui/fragment/SpotlightFragment.kt
@@ -1,120 +1,60 @@
 package com.faltenreich.release.ui.fragment
 
+import android.graphics.PointF
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.faltenreich.release.R
 import com.faltenreich.release.data.enum.ReleaseType
-import com.faltenreich.release.data.model.Release
-import com.faltenreich.release.data.viewmodel.SpotlightViewModel
-import com.faltenreich.release.extension.screenSize
-import com.faltenreich.release.extension.setImageAsync
-import com.faltenreich.release.ui.list.adapter.SpotlightListAdapter
-import com.faltenreich.release.ui.list.decoration.HorizontalPaddingDecoration
-import com.faltenreich.release.ui.view.ReleaseOpener
+import com.faltenreich.release.data.viewmodel.MainViewModel
+import com.faltenreich.release.ui.activity.BaseActivity
+import com.faltenreich.release.ui.view.TintAction
+import com.faltenreich.release.ui.viewpager.FragmentViewPagerAdapter
 import kotlinx.android.synthetic.main.fragment_spotlight.*
 
-class SpotlightFragment : BaseFragment(R.layout.fragment_spotlight), ReleaseOpener {
-    private val viewModel by lazy { createViewModel(SpotlightViewModel::class) }
-    private val type by lazy { arguments?.getSerializable(ARGUMENT_MEDIA_TYPE) as? ReleaseType }
+class SpotlightFragment : BaseFragment(R.layout.fragment_spotlight) {
+    private val parentViewModel by lazy { (activity as BaseActivity).createViewModel(MainViewModel::class) }
+    private val mediaTypes by lazy { ReleaseType.values() }
+    private lateinit var viewPagerPages: List<Pair<String, Fragment>>
+    private lateinit var viewPagerAdapter: FragmentViewPagerAdapter
 
-    private val weekListAdapter by lazy { context?.let { context -> SpotlightListAdapter(context) } }
-    private val favoriteListAdapter by lazy { context?.let { context -> SpotlightListAdapter(context) } }
-    private val recentListAdapter by lazy { context?.let { context -> SpotlightListAdapter(context) } }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initData()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initLayout()
-        fetchData()
+        initViewPager()
     }
 
-    private fun initLayout() {
-        context?.let { context ->
-            val decoration = HorizontalPaddingDecoration(context, R.dimen.margin_padding_size_small)
-
-            weekListView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-            weekListView.addItemDecoration(decoration)
-            weekListView.adapter = weekListAdapter
-
-            favoriteListView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-            favoriteListView.addItemDecoration(decoration)
-            favoriteListView.adapter = favoriteListAdapter
-
-            recentListView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-            recentListView.addItemDecoration(decoration)
-            recentListView.adapter = recentListAdapter
-        }
+    private fun initData() {
+        viewPagerPages =  mediaTypes.map { mediaType -> getString(mediaType.pluralStringRes) to SpotlightTypeFragment.newInstance(mediaType) }
+        viewPagerAdapter = FragmentViewPagerAdapter(childFragmentManager, viewPagerPages)
     }
 
-    private fun fetchData() {
-        type?.also { type ->
-            viewModel.observeWeeklyReleases(type, this) { releases ->
-                setSpotlightRelease(releases.firstOrNull())
-                setReleasesOfWeek(releases.drop(1).sortedBy(Release::releaseDate))
+    private fun initViewPager() {
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                parentViewModel.tint = TintAction(R.color.gray, PointF(event.rawX, event.rawY))
             }
-            viewModel.observeFavoriteReleases(type, this) { releases -> setFavoriteReleases(releases) }
-            viewModel.observeRecentReleases(type, this) { releases -> setRecentReleases(releases.sortedByDescending(Release::releaseDate)) }
+            false
         }
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) = Unit
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) = Unit
+            override fun onPageSelected(position: Int) {
+                setMediaType(mediaTypes[position])
+            }
+        })
+        setMediaType(mediaTypes[0])
     }
 
-    private fun setSpotlightRelease(release: Release?) {
-        context?.let { context ->
-            val hasContent = release != null
-            spotlightContainer.visibility = if (hasContent) View.VISIBLE else View.GONE
-
-            release?.imageUrlForThumbnail?.let { imageUrl -> spotlightReleaseCoverImageView.setImageAsync(imageUrl, context.screenSize.x / 2 ) } ?: spotlightReleaseCoverImageView.setImageResource(Release.FALLBACK_COVER_COLOR_RES)
-            spotlightReleaseDateTextView.text = release?.releaseDateForUi(context)
-            spotlightReleaseNameTextView.text = release?.title
-            spotlightReleaseDescriptionTextView.text = release?.description
-            spotlightContainer.setOnClickListener { release?.let { openRelease(context, it, spotlightReleaseCoverImageView) } }
-        }
-    }
-
-    private fun setReleasesOfWeek(releases: List<Release>) {
-        val hasContent = releases.isNotEmpty()
-        weekListView.isInvisible = !hasContent
-        weekEmptyView.isVisible = !hasContent
-        weekListAdapter?.let { adapter ->
-            adapter.removeListItems()
-            adapter.addListItems(releases)
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-    private fun setFavoriteReleases(releases: List<Release>) {
-        val hasContent = releases.isNotEmpty()
-        favoriteLabel.isVisible = hasContent
-        favoriteListView.isVisible = hasContent
-        favoriteListAdapter?.let { adapter ->
-            adapter.removeListItems()
-            adapter.addListItems(releases)
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-    private fun setRecentReleases(releases: List<Release>) {
-        val hasContent = releases.isNotEmpty()
-        recentListView.isInvisible = !hasContent
-        recentEmptyView.isVisible = !hasContent
-        recentListAdapter?.let { adapter ->
-            adapter.removeListItems()
-            adapter.addListItems(releases)
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-    companion object {
-        private const val ARGUMENT_MEDIA_TYPE = "mediaType"
-
-        fun newInstance(type: ReleaseType): SpotlightFragment {
-            val fragment = SpotlightFragment()
-            val arguments = Bundle()
-            arguments.putSerializable(ARGUMENT_MEDIA_TYPE, type)
-            fragment.arguments = arguments
-            return fragment
-        }
+    private fun setMediaType(releaseType: ReleaseType) {
+        parentViewModel.tint = TintAction(releaseType.colorResId)
     }
 }
---
+++ app/src/main/kotlin/com/faltenreich/release/ui/fragment/SpotlightTypeFragment.kt
@@ -0,0 +1,120 @@
+package com.faltenreich.release.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.faltenreich.release.R
+import com.faltenreich.release.data.enum.ReleaseType
+import com.faltenreich.release.data.model.Release
+import com.faltenreich.release.data.viewmodel.SpotlightViewModel
+import com.faltenreich.release.extension.screenSize
+import com.faltenreich.release.extension.setImageAsync
+import com.faltenreich.release.ui.list.adapter.SpotlightListAdapter
+import com.faltenreich.release.ui.list.decoration.HorizontalPaddingDecoration
+import com.faltenreich.release.ui.view.ReleaseOpener
+import kotlinx.android.synthetic.main.fragment_spotlight_type.*
+
+class SpotlightTypeFragment : BaseFragment(R.layout.fragment_spotlight_type), ReleaseOpener {
+    private val viewModel by lazy { createViewModel(SpotlightViewModel::class) }
+    private val type by lazy { arguments?.getSerializable(ARGUMENT_MEDIA_TYPE) as? ReleaseType }
+
+    private val weekListAdapter by lazy { context?.let { context -> SpotlightListAdapter(context) } }
+    private val favoriteListAdapter by lazy { context?.let { context -> SpotlightListAdapter(context) } }
+    private val recentListAdapter by lazy { context?.let { context -> SpotlightListAdapter(context) } }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initLayout()
+        fetchData()
+    }
+
+    private fun initLayout() {
+        context?.let { context ->
+            val decoration = HorizontalPaddingDecoration(context, R.dimen.margin_padding_size_small)
+
+            weekListView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            weekListView.addItemDecoration(decoration)
+            weekListView.adapter = weekListAdapter
+
+            favoriteListView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            favoriteListView.addItemDecoration(decoration)
+            favoriteListView.adapter = favoriteListAdapter
+
+            recentListView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            recentListView.addItemDecoration(decoration)
+            recentListView.adapter = recentListAdapter
+        }
+    }
+
+    private fun fetchData() {
+        type?.also { type ->
+            viewModel.observeWeeklyReleases(type, this) { releases ->
+                setSpotlightRelease(releases.firstOrNull())
+                setReleasesOfWeek(releases.drop(1).sortedBy(Release::releaseDate))
+            }
+            viewModel.observeFavoriteReleases(type, this) { releases -> setFavoriteReleases(releases) }
+            viewModel.observeRecentReleases(type, this) { releases -> setRecentReleases(releases.sortedByDescending(Release::releaseDate)) }
+        }
+    }
+
+    private fun setSpotlightRelease(release: Release?) {
+        context?.let { context ->
+            val hasContent = release != null
+            spotlightContainer.visibility = if (hasContent) View.VISIBLE else View.GONE
+
+            release?.imageUrlForThumbnail?.let { imageUrl -> spotlightReleaseCoverImageView.setImageAsync(imageUrl, context.screenSize.x / 2 ) } ?: spotlightReleaseCoverImageView.setImageResource(Release.FALLBACK_COVER_COLOR_RES)
+            spotlightReleaseDateTextView.text = release?.releaseDateForUi(context)
+            spotlightReleaseNameTextView.text = release?.title
+            spotlightReleaseDescriptionTextView.text = release?.description
+            spotlightContainer.setOnClickListener { release?.let { openRelease(context, it, spotlightReleaseCoverImageView) } }
+        }
+    }
+
+    private fun setReleasesOfWeek(releases: List<Release>) {
+        val hasContent = releases.isNotEmpty()
+        weekListView.isInvisible = !hasContent
+        weekEmptyView.isVisible = !hasContent
+        weekListAdapter?.let { adapter ->
+            adapter.removeListItems()
+            adapter.addListItems(releases)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun setFavoriteReleases(releases: List<Release>) {
+        val hasContent = releases.isNotEmpty()
+        favoriteLabel.isVisible = hasContent
+        favoriteListView.isVisible = hasContent
+        favoriteListAdapter?.let { adapter ->
+            adapter.removeListItems()
+            adapter.addListItems(releases)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun setRecentReleases(releases: List<Release>) {
+        val hasContent = releases.isNotEmpty()
+        recentListView.isInvisible = !hasContent
+        recentEmptyView.isVisible = !hasContent
+        recentListAdapter?.let { adapter ->
+            adapter.removeListItems()
+            adapter.addListItems(releases)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    companion object {
+        private const val ARGUMENT_MEDIA_TYPE = "mediaType"
+
+        fun newInstance(type: ReleaseType): SpotlightTypeFragment {
+            val fragment = SpotlightTypeFragment()
+            val arguments = Bundle()
+            arguments.putSerializable(ARGUMENT_MEDIA_TYPE, type)
+            fragment.arguments = arguments
+            return fragment
+        }
+    }
+}
