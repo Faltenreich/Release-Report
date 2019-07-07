f0d3a492ba59f3b8a58d4e226c219209245b5637
Faltenreich <philipp.fahlteich@gmail.com>
2019-07-07 20:16:36 +0200

    Small refactoring

--- app/src/main/kotlin/com/faltenreich/release/ui/list/layoutmanager/CalendarLayoutManager.kt
+++ app/src/main/kotlin/com/faltenreich/release/ui/list/layoutmanager/CalendarLayoutManager.kt
@@ -3,16 +3,17 @@ package com.faltenreich.release.ui.list.layoutmanager
 import android.content.Context
 import androidx.recyclerview.widget.GridLayoutManager
 import com.faltenreich.release.ui.list.adapter.CalendarListAdapter
+import org.threeten.bp.DayOfWeek
 
 class CalendarLayoutManager(
     context: Context,
     private val listAdapter: CalendarListAdapter?
-) : GridLayoutManager(context, SPAN_COUNT) {
+) : GridLayoutManager(context, DAYS_PER_WEEK) {
 
     init {
         spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
             override fun getSpanSize(position: Int): Int = when (listAdapter?.getItemViewType(position)) {
-                CalendarListAdapter.VIEW_TYPE_MONTH -> SPAN_COUNT
+                CalendarListAdapter.VIEW_TYPE_MONTH -> DAYS_PER_WEEK
                 CalendarListAdapter.VIEW_TYPE_DAY -> 1
                 else -> -1
             }
@@ -20,6 +21,6 @@ class CalendarLayoutManager(
     }
 
     companion object {
-        private const val SPAN_COUNT = 7
+        private val DAYS_PER_WEEK = DayOfWeek.values().size
     }
 }
