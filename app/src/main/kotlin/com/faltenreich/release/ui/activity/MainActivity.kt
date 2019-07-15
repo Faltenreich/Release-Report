3ff25e3007963a22f4f5569b5341cdc3719ef545
Faltenreich <philipp.fahlteich@gmail.com>
2019-07-16 00:34:23 +0200

    Fixed onBackPressed for navigation components Fixed settings for navigation

--- app/src/main/kotlin/com/faltenreich/release/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/faltenreich/release/ui/activity/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.faltenreich.release.ui.activity
 
+import androidx.navigation.findNavController
 import com.faltenreich.release.R
 
-class MainActivity : BaseActivity(R.layout.activity_main)
+class MainActivity : BaseActivity(R.layout.activity_main) {
+    override fun onBackPressed() {
+        if (!findNavController(R.id.mainNavigationHost).navigateUp()) {
+            super.onBackPressed()
+        }
+    }
+}
