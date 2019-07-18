6e07b3bc450fc7c92840ca11d94077bbbe74bdbd
Faltenreich <philipp.fahlteich@gmail.com>
2019-07-18 12:37:48 +0200

    Simplified DatePickerFragment by using navigation comoponents

--- app/src/main/kotlin/com/faltenreich/release/ui/fragment/DatePickerFragment.kt
+++ app/src/main/kotlin/com/faltenreich/release/ui/fragment/DatePickerFragment.kt
@@ -5,31 +5,22 @@ import android.app.Dialog
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import com.faltenreich.release.extension.asLocalDate
 import org.threeten.bp.LocalDate
 
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
-
-    private val givenDate by lazy { arguments?.getSerializable(DATE_GIVEN) as? LocalDate }
+    private val date by lazy { arguments?.let { arguments -> DatePickerFragmentArgs.fromBundle(arguments).date?.asLocalDate } }
 
     var onDateSet: ((LocalDate?) -> Unit)? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val date = givenDate ?: LocalDate.now()
+        val date = date ?: LocalDate.now()
         return DatePickerDialog(requireContext(), this, date.year, date.monthValue - 1, date.dayOfMonth)
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        onDateSet?.invoke(LocalDate.of(year, month + 1, dayOfMonth))
-    }
-
-    companion object {
-        private const val DATE_GIVEN = "givenDate"
-
-        fun newInstance(date: LocalDate? = null, onDateSet: (LocalDate?) -> Unit): DatePickerFragment {
-            val fragment = DatePickerFragment()
-            fragment.onDateSet = onDateSet
-            fragment.arguments = Bundle().apply { putSerializable(DATE_GIVEN, date) }
-            return fragment
-        }
+        val date = LocalDate.of(year, month + 1, dayOfMonth)
+        onDateSet?.invoke(date)
+        // TODO: Propagate date change to potential observers
     }
 }
