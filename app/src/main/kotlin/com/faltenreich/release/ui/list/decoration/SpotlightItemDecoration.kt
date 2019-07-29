6d37044743eb966c2de5356b5c71121afd62bc35
Faltenreich <philipp.fahlteich@gmail.com>
2019-07-29 21:48:37 +0200

    Shrinked spacing for spotlight item decoration

--- app/src/main/kotlin/com/faltenreich/release/ui/list/decoration/SpotlightItemDecoration.kt
+++ app/src/main/kotlin/com/faltenreich/release/ui/list/decoration/SpotlightItemDecoration.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.faltenreich.release.R
 import com.faltenreich.release.ui.list.adapter.SpotlightListAdapter
 import com.faltenreich.release.ui.list.item.SpotlightItem
 import com.faltenreich.release.ui.list.item.SpotlightReleaseItem
 
 class SpotlightItemDecoration(context: Context) : LinearLayoutItemDecoration<SpotlightItem, SpotlightListAdapter>() {
-    private val spacing: Int = context.resources.getDimension(R.dimen.margin_padding_size_medium).toInt()
+    private val spacing: Int = context.resources.getDimension(SPACING_RES_DEFAULT).toInt()
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
