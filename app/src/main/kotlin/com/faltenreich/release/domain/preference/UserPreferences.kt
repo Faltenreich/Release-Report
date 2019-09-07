8a0e38227c0c5c63292679ca60ea11f1a562450a
Faltenreich <philipp.fahlteich@gmail.com>
2019-09-07 10:37:25 +0200

    Set default reminder time to 9 o' clock

--- app/src/main/kotlin/com/faltenreich/release/domain/preference/UserPreferences.kt
+++ app/src/main/kotlin/com/faltenreich/release/domain/preference/UserPreferences.kt
@@ -10,12 +10,14 @@ object UserPreferences : SharedPreferences() {
     private const val KEY_FAVORITE_RELEASE_IDS = "favoriteReleaseIds"
     private const val KEY_REMINDER_TIME = "reminderTime"
 
+    private val REMINDER_TIME_DEFAULT = LocalTime.of(9, 0)
+
     var favoriteReleaseIds: Set<String>
         get() = getStringSet(KEY_FAVORITE_RELEASE_IDS)
         set(value) = setStringSet(KEY_FAVORITE_RELEASE_IDS to value)
 
     var reminderTime: LocalTime
-        get() = get<String>(KEY_REMINDER_TIME).takeIf(String::isNotBlank)?.asLocalTime ?: LocalTime.now()
+        get() = get<String>(KEY_REMINDER_TIME).takeIf(String::isNotBlank)?.asLocalTime ?: REMINDER_TIME_DEFAULT
         set(value) = set(KEY_REMINDER_TIME to value.asString)
 
     fun registerOnSharedPreferenceChangeListener(listener: OnSharedPreferenceChangeListener) {
