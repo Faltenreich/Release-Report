1cbdf2e01bf66c42763d7916bebc260a273aaa4f
Faltenreich <philipp.fahlteich@gmail.com>
2020-01-19 17:53:44 +0100

    Added progress view to image list Improved layout of release detail

--- app/src/main/kotlin/com/faltenreich/release/domain/release/detail/ImageViewHolder.kt
+++ app/src/main/kotlin/com/faltenreich/release/domain/release/detail/ImageViewHolder.kt
@@ -2,11 +2,13 @@ package com.faltenreich.release.domain.release.detail
 
 import android.content.Context
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.faltenreich.release.R
 import com.faltenreich.release.base.intent.UrlOpener
 import com.faltenreich.release.framework.android.view.setImageAsync
 import com.faltenreich.release.framework.android.viewholder.BaseViewHolder
-import kotlinx.android.synthetic.main.list_item_release_promo.*
+import kotlinx.android.synthetic.main.list_item_image.*
+import kotlinx.android.synthetic.main.list_item_release_promo.imageView
 
 class ImageViewHolder(
     context: Context,
@@ -22,6 +24,9 @@ class ImageViewHolder(
     }
 
     override fun onBind(data: String) {
-        imageView.setImageAsync(data, imageView.width)
+        progressView.isVisible = true
+        imageView.setImageAsync(data, imageView.width) {
+            progressView.isVisible = false
+        }
     }
 }
