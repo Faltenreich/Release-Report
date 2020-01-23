6960db15655a46a0fc015c94ec0a2da0d0bc7f10
Faltenreich <philipp.fahlteich@gmail.com>
2020-01-23 20:08:44 +0100

    Removed placeholder image from ScrollGalleryView by implementing custom GlideImageLoader

--- app/src/main/kotlin/com/faltenreich/release/domain/media/ImageGalleryFragment.kt
+++ app/src/main/kotlin/com/faltenreich/release/domain/media/ImageGalleryFragment.kt
@@ -2,14 +2,17 @@ package com.faltenreich.release.domain.media
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.doOnPreDraw
 import androidx.viewpager.widget.ViewPager
 import com.faltenreich.release.R
 import com.faltenreich.release.framework.android.fragment.BaseFragment
 import com.faltenreich.release.framework.android.view.fitSystemWindows
+import com.faltenreich.release.framework.scrollgalleryview.GlideImageLoader
+import com.veinhorn.scrollgalleryview.MediaInfo
 import com.veinhorn.scrollgalleryview.ScrollGalleryView
 import com.veinhorn.scrollgalleryview.builder.GallerySettings
 import kotlinx.android.synthetic.main.fragment_image_gallery.*
-import ogbe.ozioma.com.glideimageloader.dsl.DSL
 
 class ImageGalleryFragment : BaseFragment(R.layout.fragment_image_gallery) {
 
@@ -17,10 +20,12 @@ class ImageGalleryFragment : BaseFragment(R.layout.fragment_image_gallery) {
     private val imageUrl by lazy { ImageGalleryFragmentArgs.fromBundle(requireArguments()).imageUrl }
 
     private lateinit var scrollGalleryView: ScrollGalleryView
+    private lateinit var thumbnailsContainer: ViewGroup
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initLayout()
+        selectImageUrl(imageUrl)
     }
 
     private fun initLayout() {
@@ -32,6 +37,7 @@ class ImageGalleryFragment : BaseFragment(R.layout.fragment_image_gallery) {
             .thumbnailSize(THUMBNAIL_SIZE)
             .enableZoom(true)
             .build()
+        val media = imageUrls.map { imageUrl -> MediaInfo.mediaLoader(GlideImageLoader(imageUrl)) }
 
         scrollGalleryView = ScrollGalleryView
             .from(galleryView)
@@ -41,16 +47,19 @@ class ImageGalleryFragment : BaseFragment(R.layout.fragment_image_gallery) {
                 override fun onPageSelected(position: Int) = Unit
             })
             .settings(settings)
-            .apply {
-                imageUrls.forEach { imageUrl -> add(DSL.image(imageUrl)) }
-            }
+            .add(media)
             .build()
+        thumbnailsContainer = scrollGalleryView.findViewById(com.veinhorn.scrollgalleryview.R.id.thumbnails_container)
+    }
 
-        // FIXME: Does not scroll to thumbnail
-        scrollGalleryView.currentItem = imageUrls.indexOf(imageUrl)
+    private fun selectImageUrl(imageUrl: String) {
+        val index = imageUrls.indexOf(imageUrl)
+        scrollGalleryView.currentItem = index
+        // Workaround: ScrollGalleryView.setCurrentItem() does not scroll to Thumbnail
+        thumbnailsContainer.getChildAt(index).doOnPreDraw { thumbnail -> thumbnail.performClick() }
     }
 
     companion object {
-        private const val THUMBNAIL_SIZE = 300
+        private const val THUMBNAIL_SIZE = 400
     }
 }
---
+++ app/src/main/kotlin/com/faltenreich/release/framework/scrollgalleryview/GlideImageLoader.java
@@ -0,0 +1,71 @@
+package com.faltenreich.release.framework.scrollgalleryview;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+import com.veinhorn.scrollgalleryview.loader.MediaLoader;
+
+public class GlideImageLoader implements MediaLoader {
+
+    private String url;
+    private RequestOptions requestOptions;
+
+    public GlideImageLoader(String url) {
+        this.url = url;
+        requestOptions = new RequestOptions();
+    }
+
+    public GlideImageLoader(String url, int width, int height) {
+        this.url = url;
+        requestOptions = new RequestOptions().override(width, height);
+    }
+
+    @Override
+    public boolean isImage() {
+        return true;
+    }
+
+    @Override
+    public void loadMedia(Context context, ImageView imageView, final SuccessCallback callback) {
+        RequestOptions requestOptions = new RequestOptions();
+        Glide.with(context).applyDefaultRequestOptions(requestOptions)
+                .load(url).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                callback.onSuccess();
+                return false;
+            }
+        }).into(imageView);
+    }
+
+    @Override
+    public void loadThumbnail(Context context, ImageView thumbnailView, final SuccessCallback callback) {
+        Glide.with(context).applyDefaultRequestOptions(requestOptions).load(url).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                callback.onSuccess();
+                return false;
+            }
+        }).into(thumbnailView);
+    }
+
+}
