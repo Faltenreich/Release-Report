0b986b20ffeff14c8eb21e600547a85751b088fe
Faltenreich <philipp.fahlteich@gmail.com>
2020-02-05 21:03:08 +0100

    Replaced favorite icon with first cover in calendar Indicate today via typeface instead of background color

--- app/src/main/kotlin/com/faltenreich/release/domain/release/calendar/CalendarDayViewHolder.kt
+++ app/src/main/kotlin/com/faltenreich/release/domain/release/calendar/CalendarDayViewHolder.kt
@@ -1,13 +1,15 @@
 package com.faltenreich.release.domain.release.calendar
 
 import android.content.Context
+import android.graphics.Typeface
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.faltenreich.release.R
 import com.faltenreich.release.base.date.isToday
-import com.faltenreich.release.base.primitive.isTrue
 import com.faltenreich.release.domain.date.DateOpener
+import com.faltenreich.release.framework.android.view.setImageAsync
 import com.faltenreich.release.framework.android.viewholder.BaseViewHolder
 import kotlinx.android.synthetic.main.list_item_calendar_day.*
 
@@ -20,24 +22,34 @@ class CalendarDayViewHolder(
     parent
 ), DateOpener {
 
+    init {
+        container.setOnClickListener { openDate(navigationController, data.date) }
+    }
+
     override fun onBind(data: CalendarDayItem) {
         val (date, _, releases) = data
         val isToday = date.isToday
         val isInSameMonth = data.isInSameMonth
-        val containsSubscription = releases?.any { release -> release.isSubscribed }.isTrue
-
-        clickable.setOnClickListener { openDate(navigationController, date) }
 
         dayLabel.text = date.dayOfMonth.toString()
-        dayLabel.setTextColor(ContextCompat.getColor(context,
-            if (isInSameMonth) android.R.color.white
-            else R.color.gray
-        ))
-
-        container.setBackgroundResource(
-            if (isInSameMonth && isToday) R.color.colorPrimaryDarker
-            else android.R.color.transparent
+        dayLabel.setTextColor(
+            ContextCompat.getColor(
+                context,
+                if (isInSameMonth) android.R.color.white
+                else R.color.gray
+            )
         )
-        subscriptionIcon.isVisible = isInSameMonth && containsSubscription
+        dayLabel.setTypeface(
+            dayLabel.typeface,
+            if (isToday) Typeface.BOLD else Typeface.NORMAL
+        )
+
+        val subscription = releases?.firstOrNull { release -> release.isSubscribed }
+        subscription?.imageUrlForCover?.let { url ->
+            coverView.isVisible = true
+            coverView.setImageAsync(url, itemView.width)
+        } ?: run {
+            coverView.isGone = true
+        }
     }
 }
