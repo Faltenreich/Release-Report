6ab94aea94ae805f92af983eda08852a18b326a3
Faltenreich <philipp.fahlteich@gmail.com>
2020-02-10 18:28:06 +0100

    Refactored view model for spotlight

--- app/src/main/kotlin/com/faltenreich/release/domain/release/spotlight/SpotlightViewModel.kt
+++ app/src/main/kotlin/com/faltenreich/release/domain/release/spotlight/SpotlightViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.viewModelScope
 import com.faltenreich.release.R
 import com.faltenreich.release.base.date.atEndOfWeek
 import com.faltenreich.release.base.date.atStartOfWeek
-import com.faltenreich.release.data.model.Release
 import com.faltenreich.release.data.repository.ReleaseRepository
 import com.faltenreich.release.domain.release.list.ReleaseItem
 import com.faltenreich.release.framework.androidx.LiveDataFix
@@ -33,73 +32,41 @@ class SpotlightViewModel : ViewModel() {
             val (startOfWeek, endOfWeek) = today.atStartOfWeek to today.atEndOfWeek
 
             // Plus one for the promo
-            val weekly = ReleaseRepository.getBetween(startOfWeek, endOfWeek, PAGE_SIZE + 1)
-
-            val lastMonth = today.minusMonths(1)
-            val endOfLastWeek = startOfWeek.minusDays(1)
-            val recent = ReleaseRepository.getBetween(lastMonth, endOfLastWeek, PAGE_SIZE)
-
-            val subscriptions = ReleaseRepository.getSubscriptions(LocalDate.now(), PAGE_SIZE)
-
-            setData(
-                weekly.sortedByDescending(Release::popularity),
-                recent.sortedByDescending(Release::popularity),
-                subscriptions
-            )
-        }
-    }
-
-    private fun setData(weekly: List<Release>, recent: List<Release>, subscriptions: List<Release>) {
-        val items = mutableListOf<SpotlightItem>()
-
-        addPromo(weekly, items)
-        addSubscriptions(subscriptions, items)
-        addWeekly(weekly, items)
-        addRecent(recent, items)
-
-        spotlightItems = items.toList()
-    }
-
-    private fun addPromo(from: List<Release>, to: MutableList<SpotlightItem>) {
-        from.firstOrNull()?.let { release -> to.add(SpotlightPromoItem(release)) }
-    }
-
-    private fun addSubscriptions(from: List<Release>, to: MutableList<SpotlightItem>) {
-        from.takeIf(List<*>::isNotEmpty)?.let { releases ->
-            to.add(
-                SpotlightReleaseItem(
-                    R.string.for_you,
-                    releases.mapNotNull { release ->
-                        release.releaseDate?.let { date -> ReleaseItem(release, date) }
-                    }
-                )
-            )
-        }
-    }
-
-    private fun addWeekly(from: List<Release>, to: MutableList<SpotlightItem>) {
-        from.drop(1).takeIf(List<*>::isNotEmpty)?.let { releases ->
-            to.add(
+            val weeklyReleases = ReleaseRepository.getBetween(startOfWeek, endOfWeek, PAGE_SIZE + 1)
+            val promo = weeklyReleases.firstOrNull()?.let { release -> SpotlightPromoItem(release) }
+            val weekly = weeklyReleases.drop(1).takeIf(List<*>::isNotEmpty)?.let { releases ->
                 SpotlightReleaseItem(
                     R.string.this_week,
                     releases.mapNotNull { release ->
                         release.releaseDate?.let { date -> ReleaseItem(release, date) }
                     }
                 )
-            )
-        }
-    }
+            }
 
-    private fun addRecent(from: List<Release>, to: MutableList<SpotlightItem>) {
-        from.takeIf(List<*>::isNotEmpty)?.let { releases ->
-            to.add(
+            val subscriptionReleases =
+                ReleaseRepository.getSubscriptions(LocalDate.now(), PAGE_SIZE)
+            val subscription = subscriptionReleases.takeIf(List<*>::isNotEmpty)?.let { releases ->
+                SpotlightReleaseItem(
+                    R.string.for_you,
+                    releases.mapNotNull { release ->
+                        release.releaseDate?.let { date -> ReleaseItem(release, date) }
+                    }
+                )
+            }
+
+            val lastMonth = today.minusMonths(1)
+            val endOfLastWeek = startOfWeek.minusDays(1)
+            val recentReleases = ReleaseRepository.getBetween(lastMonth, endOfLastWeek, PAGE_SIZE)
+            val recent = recentReleases.takeIf(List<*>::isNotEmpty)?.let { releases ->
                 SpotlightReleaseItem(
                     R.string.recently,
                     releases.mapNotNull { release ->
                         release.releaseDate?.let { date -> ReleaseItem(release, date) }
                     }
                 )
-            )
+            }
+
+            spotlightItems = listOfNotNull(promo, weekly, subscription, recent)
         }
     }
 
