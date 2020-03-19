c189c7da66b34a538d24a784e46843e2ced53c5b
Faltenreich <philipp.fahlteich@gmail.com>
2020-03-19 14:31:30 +0100

    Fixed build break by removing unused extension

--- app/src/main/kotlin/com/faltenreich/release/framework/android/activity/ActivityExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.faltenreich.release.framework.android.activity
-
-import android.app.Activity
-import android.view.View
-import com.faltenreich.release.framework.android.context.hideKeyboard
-
-fun Activity.hideKeyboard() {
-    hideKeyboard(if (currentFocus == null) View(this) else currentFocus)
-}
