c448d68c98d2255a44a70e2b1fdf746d52cf1014
Faltenreich <philipp.fahlteich@gmail.com>
2020-08-01 11:06:12 +0200

    Fake current date for testing reminder

--- app/src/androidTest/kotlin/com/faltenreich/release/domain/reminder/ReminderTest.kt
+++ app/src/androidTest/kotlin/com/faltenreich/release/domain/reminder/ReminderTest.kt
@@ -5,12 +5,16 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.work.ListenableWorker
 import androidx.work.testing.TestListenableWorkerBuilder
+import com.faltenreich.release.base.date.Now
+import com.faltenreich.release.base.date.atStartOfWeek
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.threeten.bp.Clock
+import org.threeten.bp.ZoneOffset
 
 @RunWith(AndroidJUnit4::class)
 class ReminderTest {
@@ -20,6 +24,8 @@ class ReminderTest {
     @Before
     fun setup() {
         context = ApplicationProvider.getApplicationContext()
+        val startOfWeek = Now.localDate().atStartOfWeek.atTime(8, 0).toInstant(ZoneOffset.UTC)
+        Now.clock = Clock.fixed(startOfWeek, ZoneOffset.UTC)
     }
 
     @Test
