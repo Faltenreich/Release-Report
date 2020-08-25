6cb221d526f2605f9fe5d3a9594257b9e5955b74
Faltenreich <philipp.fahlteich@gmail.com>
2020-08-25 20:24:36 +0200

    Simplified image placeholder

--- app/src/main/kotlin/com/faltenreich/release/domain/release/ReleaseExtensions.kt
+++ app/src/main/kotlin/com/faltenreich/release/domain/release/ReleaseExtensions.kt
@@ -1,35 +1,17 @@
 package com.faltenreich.release.domain.release
 
-import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
-import com.faltenreich.release.R
 import com.faltenreich.release.data.model.Release
-import com.faltenreich.release.framework.android.context.getColorFromAttribute
 import com.faltenreich.release.framework.glide.setImageAsync
 
 private fun ImageView.setImage(
     url: String?,
     callback: ((Drawable?) -> Unit)?
 ) {
-    val placeholder = ColorDrawable(context.getColorFromAttribute(R.attr.backgroundColorSecondary))
     url?.let {
-        setImageAsync(url, placeholder) { drawable ->
-            if (drawable == null) {
-                onImageNotFound(placeholder, callback)
-            } else {
-                callback?.invoke(drawable)
-            }
-        }
-    } ?: onImageNotFound(placeholder, callback)
-}
-
-private fun ImageView.onImageNotFound(
-    placeholder: Drawable,
-    callback: ((Drawable?) -> Unit)?
-) {
-    setImageDrawable(placeholder)
-    callback?.invoke(null)
+        setImageAsync(url, null, callback)
+    } ?: callback?.invoke(null)
 }
 
 fun ImageView.setWallpaper(
